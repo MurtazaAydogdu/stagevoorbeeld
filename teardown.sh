4198d03ab4dcde5335e142a559f6dd4625680b0c
Murtaza Aydogdu <murtazaaydogdu@MBP-van-Murtaza.fritz.box>
2018-04-25 16:41:29 +0200

    changed microstore to microservicetransactions

--- teardown.sh
+++ teardown.sh
@@ -3,6 +3,6 @@
 echo "cleanup"
 # git tag is run as root from container... correct permissions on workspace
 sudo chown -R jenkins:jenkins .git
-docker-compose -p smartengine down --volumes --remove-orphans
+docker-compose -p microservicetransactions down --volumes --remove-orphans
 docker ps -aq |xargs docker rm
 docker volume ls -q |xargs docker volume rm
