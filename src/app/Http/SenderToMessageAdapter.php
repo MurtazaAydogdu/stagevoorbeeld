3278bbce06232ddc198bc5b4f6f98d035656518a
Murtaza Aydogdu <murtazaaydogdu@MBP-van-Murtaza.fritz.box>
2018-06-25 10:17:42 +0200

    ry catch to catch the error

--- src/app/Http/SenderToMessageAdapter.php
+++ src/app/Http/SenderToMessageAdapter.php
@@ -6,17 +6,23 @@ class SenderToMessageAdapter {
 
 
     public function send($type, $url, $status, $origin, $data) {
-        $data = array("data" => $data);                                                                    
-        $data_string = json_encode($data);                                                                                   
-                                                                                                                            
-        $ch = curl_init(env('MESSAGE_ADAPTER') . $url . '?status=' . $status . '&origin=' . $origin);                                                                      
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);                                                                     
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-            'Content-Type: application/json'                                                                         
-        ));                                                                                                                   
-                                                                                                                            
-        $result = curl_exec($ch);
+       try {
+            $data = array("data" => $data);                                                                    
+            $data_string = json_encode($data);                                                                                   
+                                                                                                                                
+            $ch = curl_init(env('MESSAGE_ADAPTER') . $url . '?status=' . $status . '&origin=' . $origin);                                                                      
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);                                                                     
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                'Content-Type: application/json'                                                                         
+            ));                                                                                                                   
+                                                                                                                                
+            $result = curl_exec($ch);
+       }
+
+       catch(\Exception $e) {
+           echo $e->getMessage();
+       }
     }
 }
