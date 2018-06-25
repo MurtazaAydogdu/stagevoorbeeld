f8f8903cd457d297314aa87dc0b5e7297cad77e7
Murtaza Aydogdu <murtazaaydogdu@MBP-van-Murtaza.fritz.box>
2018-06-25 11:07:07 +0200

    return resopnse body

--- src/app/Http/SenderToMessageAdapter.php
+++ src/app/Http/SenderToMessageAdapter.php
@@ -12,13 +12,20 @@ class SenderToMessageAdapter {
         $ch = curl_init(env('MESSAGE_ADAPTER') . $url . '?status=' . $status . '&origin=' . $origin);                                                                      
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);                                                                     
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);       
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);                                                               
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
             'Content-Type: application/json'                                                                         
-        ));                                                                                                                   
-                                                                                                                            
-        $result = curl_exec($ch);
+        ));                                               
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+                                                                                                                                                                                  
+        $response = curl_exec($ch);
 
-        return $result;
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+
+
+        return $body;
     }
 }
