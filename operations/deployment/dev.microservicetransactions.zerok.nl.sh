4688c0108025b9e17085a072230de387ee1ba027
MurtazaAydogdu <s1094770@student.hsleiden.nl>
2018-06-27 15:23:28 +0200

    dev.vhost

---
+++ operations/deployment/dev.microservicetransactions.zerok.nl.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+current_dir=$(cd $(dirname $0) && pwd)
+cd ${current_dir}
+
+ADAPTER_CONFIG=$(cat $PWD/etc/config.json | base64)
+VHOST=dev.microservicetransactions.zerok.nl
+
+./deploy.sh root@192.168.0.127 ${VHOST} 
