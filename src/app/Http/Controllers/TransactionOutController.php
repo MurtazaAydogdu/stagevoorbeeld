0ad81de2c7141b16dad6ff1aaafd2a7aec709eb8
MurtazaAydogdu <s1094770@student.hsleiden.nl>
2018-07-05 14:35:01 +0200

    edit description

--- src/app/Http/Controllers/TransactionOutController.php
+++ src/app/Http/Controllers/TransactionOutController.php
@@ -493,7 +493,7 @@ class TransactionOutController extends ApiController
             $transaction->subscription_id = $subscription_id;
             $transaction->product_id = $product_id;
             $transaction->amount = $price;
-            $transaction->description = !empty($description) ? $description : "";
+            $transaction->description = !empty($description) ? $description : "No subscription given.";
             $transaction->origin = $origin;
             $saved = $transaction->save();
 
